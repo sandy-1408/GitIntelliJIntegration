3d57a6fcfbfb89df195f9787942a5512c2198211
sandhya <sandhyamadire305@gmail.com>
2024-10-14 20:58:11 -0500

    Third Commit in Master Branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("Second commit in Master branch");
+        System.out.println("Third commit in Master branch");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
