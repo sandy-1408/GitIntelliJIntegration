f92cba284d65ce7e78f8561c4d5eeb9ead77c51c
sandy-1408 <sandhya.m@mycvhire.com>
2024-10-15 10:15:23 -0500

    Merge conflict demo -1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,7 +16,7 @@ public class Main {
         System.out.println(" Git pull Example in local repo");
         System.out.println(" Edit a commit message -1");
         System.out.println("Reset Branch to specific commit - 1");
-        
+        System.out.println("Merge conflict demo -1 ");
         
 
 
