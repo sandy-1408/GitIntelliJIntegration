31f953cc3f5425b4039a303647239c91612a66de
sandhya <sandhyamadire305@gmail.com>
2024-10-16 13:27:18 -0500

    first commit master branch_updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -36,6 +36,8 @@ public class Main {
         System.out.println("second commit in feature2 branch....");
 
 
+        System.out.println("first commit master branch_updated....");
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
